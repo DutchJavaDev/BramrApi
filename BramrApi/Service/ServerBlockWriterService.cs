7bab878edf66b590e9ecc04711c00fb26397a164
Boris <Borismulder@outlook.com>
2020-11-27 20:34:29 +0100

    Create ServerBlockWriterService.cs
    Base for creating server blocks in nginx config file

---
+++ BramrApi/Service/ServerBlockWriterService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using BramrApi.Service.Interfaces;
+
+namespace BramrApi.Service
+{
+    public class ServerBlockWriterService : IServerBlockWriterService
+    {
+#if DEBUG
+        private string CONFIG_FILE_PATH = @$"{Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory)}";
+#else
+        private const string CONFIG_FILE_PATH = @"/etc/nginx/sites-available/bramr.tech";
+#endif
+        public string LoadFile(string fileName)
+        {
+            return string.Empty;
+        }
+
+        public string CreateServerBlock(string url, string alias)
+        {
+            return string.Empty;
+        }
+
+        public string FindServerBlock(string name, string[] lines, StringBuilder trimmedFile)
+        {
+            return string.Empty;
+        }
+    }
+}
