3ee346cd07952c4c12c0627113efb622678a9dd3
Boris <Borismulder@outlook.com>
2020-11-27 20:34:58 +0100

    Create IServerBlockWriterService.cs
    Base interface for service

---
+++ BramrApi/Service/Interfaces/IServerBlockWriterService.cs
@@ -0,0 +1,8 @@
+﻿namespace BramrApi.Service.Interfaces
+{
+    public interface IServerBlockWriterService
+    {
+        string LoadFile(string fileName);
+        string CreateServerBlock(string url, string alias);
+    }
+}
