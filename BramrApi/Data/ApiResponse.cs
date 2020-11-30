7f5037f809cbc606f08bddbcaea3d274f87ec9c5
Boris <Borismulder@outlook.com>
2020-11-30 21:33:10 +0100

    Create ApiResponse.cs
    easy...

---
+++ BramrApi/Data/ApiResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace BramrApi.Data
+{
+    public class ApiResponse
+    {
+        private ApiResponse() { }
+
+        public bool Success { get; private set; } = false;
+
+        public string Message { get; private set; } = string.Empty;
+
+        public object RequestedData { get; private set; } = string.Empty;
+
+        public static ApiResponse Oke(string message = "", object data = null)
+        {
+            if (data == null)
+                return new ApiResponse { Success = true, Message = message };
+            else
+                return new ApiResponse { Success = true, Message = message, RequestedData = data };
+        }
+
+        public static ApiResponse Error(string message = "", object data = null)
+        {
+            return new ApiResponse { Success = false, Message = message };
+        }
+    }
+}
