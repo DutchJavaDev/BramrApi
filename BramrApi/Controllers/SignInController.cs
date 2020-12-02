fa02e6d6bd103eac90f73be8db73325864a30141
Boris <Borismulder@outlook.com>
2020-12-02 16:00:43 +0100

    Create SignInController.cs
    Base controller for logging in with the api

---
+++ BramrApi/Controllers/SignInController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using BramrApi.Data;
+
+namespace BramrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignInController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public SignInController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task<ApiResponse> Login([FromBody] LoginModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (await userManager.FindByEmailAsync(model.Email) == null)
+                    return ApiResponse.Error("User not found!");
+
+
+            }
+
+            return ApiResponse.Error("Not all required fields have been filled in");
+        }
+
+    }
+}
