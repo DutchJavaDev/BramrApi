a83a54614b763ce793b7754702a71b505bd53f62
Boris <Borismulder@outlook.com>
2020-12-03 10:53:18 +0100

    Mathijs

---
+++ BramrApi/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using BramrApi.Service.Interfaces;
+using BramrApi.Data;
+using System;
+using System.Threading.Tasks;
+using BramrApi.Database.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BramrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IDatabase database;
+
+        public TestController(IDatabase database)
+        {
+            this.database = database;
+        }
+
+
+        [HttpPost("db")]
+        public async Task<ApiResponse> DbTest([FromForm] Auto auto)
+        {
+            await database.AddModel(auto);
+
+            return ApiResponse.Oke(data: database.GetAllModels<Auto>());
+        }
+    }
+}
--- BramrApi/Database/DatabaseMapper.cs
+++ BramrApi/Database/DatabaseMapper.cs
@@ -1,4 +1,19 @@
-﻿namespace BramrApi.Database
-{
+﻿using BramrApi.Database.Data;
+using FluentNHibernate.Mapping;
 
+namespace BramrApi.Database
+{
+    public class AutoMapper : ClassMap<Auto>
+    {
+        public AutoMapper()
+        {
+            Table("auto_table");
+            Id(model => model.Id);
+            Map(model => model.Jaar);
+            Map(model => model.Prijs);
+            Map(model => model.Merk);
+            Map(model => model.CreationDate);
+            Map(model => model.BouwDatum);
+        }
+    }
 }
