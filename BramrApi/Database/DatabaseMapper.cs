c5f72a2bdfcf38345fdba9d38c3a9c709ce57f43
Boris <Borismulder@outlook.com>
2020-12-07 10:19:33 +0100

    Delete test .....

--- BramrApi/Database/DatabaseMapper.cs
+++ BramrApi/Database/DatabaseMapper.cs
@@ -3,17 +3,4 @@ using FluentNHibernate.Mapping;
 
 namespace BramrApi.Database
 {
-    public class AutoMapper : ClassMap<Auto>
-    {
-        public AutoMapper()
-        {
-            Table("auto_table");
-            Id(model => model.Id);
-            Map(model => model.Jaar);
-            Map(model => model.Prijs);
-            Map(model => model.Merk);
-            Map(model => model.CreationDate);
-            Map(model => model.BouwDatum);
-        }
-    }
 }
