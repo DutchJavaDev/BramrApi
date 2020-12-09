c1a17e45eba6b462448703e02d2d590b7f554ed3
Boris <Borismulder@outlook.com>
2020-12-09 09:59:46 +0100

    Update SignUpController.cs

--- BramrApi/Controllers/SignUpController.cs
+++ BramrApi/Controllers/SignUpController.cs
@@ -63,15 +63,15 @@ namespace BramrApi.Controllers
                 };
 
                 // Gen password?
-                // var password = GenerateRandomPassword();
+                 var password = GenerateRandomPassword();
 
                 // Create a user with function
-                var result = await UserManager.CreateAsync(user, model.Password);
+                var result = await UserManager.CreateAsync(user, password);
 
                 if (result.Succeeded)
                 {
                     // TODO mail password?, ask group
-                    //await mailClient.SendPasswordEmail(model.Email,"");
+                    await mailClient.SendPasswordEmail(model.Email,password);
 
                     //👋
                     return ApiResponse.Oke("Account has been created");
@@ -99,45 +99,45 @@ namespace BramrApi.Controllers
 
         //Stole this from stackoverflow, might not use it. Ask group
 
-       //[NonAction]
-       // private string GenerateRandomPassword()
-       // {
-       //     string[] randomChars = new[] {
-       //     "ABCDEFGHJKLMNOPQRSTUVWXYZ",    // uppercase 
-       //     "abcdefghijkmnopqrstuvwxyz",    // lowercase
-       //     "0123456789",                   // digits
-       //     "!@$?_-"                        // non-alphanumeric
-       // };
+        [NonAction]
+        private string GenerateRandomPassword()
+        {
+            string[] randomChars = new[] {
+            "ABCDEFGHJKLMNOPQRSTUVWXYZ",    // uppercase 
+            "abcdefghijkmnopqrstuvwxyz",    // lowercase
+            "0123456789",                   // digits
+            "!@$?_-"                        // non-alphanumeric
+        };
 
-       //     var rand = new Random(Environment.TickCount);
-       //     List<char> chars = new List<char>();
+            var rand = new Random(Environment.TickCount);
+            List<char> chars = new List<char>();
 
-       //     if (PasswordOptions.RequireUppercase)
-       //         chars.Insert(rand.Next(0, chars.Count),
-       //             randomChars[0][rand.Next(0, randomChars[0].Length)]);
+            if (PasswordOptions.RequireUppercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[0][rand.Next(0, randomChars[0].Length)]);
 
-       //     if (PasswordOptions.RequireLowercase)
-       //         chars.Insert(rand.Next(0, chars.Count),
-       //             randomChars[1][rand.Next(0, randomChars[1].Length)]);
+            if (PasswordOptions.RequireLowercase)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[1][rand.Next(0, randomChars[1].Length)]);
 
-       //     if (PasswordOptions.RequireDigit)
-       //         chars.Insert(rand.Next(0, chars.Count),
-       //             randomChars[2][rand.Next(0, randomChars[2].Length)]);
+            if (PasswordOptions.RequireDigit)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[2][rand.Next(0, randomChars[2].Length)]);
 
-       //     if (PasswordOptions.RequireNonAlphanumeric)
-       //         chars.Insert(rand.Next(0, chars.Count),
-       //             randomChars[3][rand.Next(0, randomChars[3].Length)]);
+            if (PasswordOptions.RequireNonAlphanumeric)
+                chars.Insert(rand.Next(0, chars.Count),
+                    randomChars[3][rand.Next(0, randomChars[3].Length)]);
 
-       //     for (int i = chars.Count; i < PasswordOptions.RequiredLength
-       //         || chars.Distinct().Count() < PasswordOptions.RequiredUniqueChars; i++)
-       //     {
-       //         string rcs = randomChars[rand.Next(0, randomChars.Length)];
-       //         chars.Insert(rand.Next(0, chars.Count),
-       //             rcs[rand.Next(0, rcs.Length)]);
-       //     }
+            for (int i = chars.Count; i < PasswordOptions.RequiredLength
+                || chars.Distinct().Count() < PasswordOptions.RequiredUniqueChars; i++)
+            {
+                string rcs = randomChars[rand.Next(0, randomChars.Length)];
+                chars.Insert(rand.Next(0, chars.Count),
+                    rcs[rand.Next(0, rcs.Length)]);
+            }
 
-       //     return new string(chars.ToArray());
-       // }
+            return new string(chars.ToArray());
+        }
 
     }
 }
