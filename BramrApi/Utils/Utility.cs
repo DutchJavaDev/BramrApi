142e2deb4c9ed137e402aafe6071c20fe3add60d
Boris <Borismulder@outlook.com>
2020-12-10 16:03:06 +0100

    Create Utility.cs

---
+++ BramrApi/Utils/Utility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BramrApi.Utils
+{
+    public static class Utility
+    {
+        public static string CreateFileUri()
+        {
+            string Uri = Guid.NewGuid().ToString().Replace("-", "");
+            return Uri;
+        }
+    }
+}
