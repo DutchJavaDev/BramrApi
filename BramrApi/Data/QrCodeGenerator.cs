38dd8c34dc81916e6d11224b5ffd3e0210e16822
Boris <Borismulder@outlook.com>
2020-12-10 16:02:55 +0100

    Update QrCodeGenerator.cs

--- BramrApi/Data/QrCodeGenerator.cs
+++ BramrApi/Data/QrCodeGenerator.cs
@@ -12,7 +12,7 @@ namespace BramrApi.Data
 {
     public class QrCodeGenerator
     {
-        public void CreateQR(string url)
+        public void CreateQR(string url, string filename)
         {
             Url generator = new Url(url);
             string payload = generator.ToString();
@@ -20,12 +20,22 @@ namespace BramrApi.Data
             using QRCodeData qRCodeData = qrg.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
             using QRCode qrCode = new QRCode(qRCodeData);
             using var bitmap = qrCode.GetGraphic(5, Color.DarkBlue, Color.White, true);
+
+#if DEBUG
             if (!Directory.Exists(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\temp\"))
             {
                 Directory.CreateDirectory(@$"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\temp\");
             }
 
-            bitmap.Save($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\temp\qrCode.jpeg", ImageFormat.Jpeg);
+            bitmap.Save($@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\temp\{filename}.jpeg", ImageFormat.Jpeg);
+#else
+            if (!Directory.Exists(@$"{Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)}\temp\"))
+            {
+                Directory.CreateDirectory(@$"{Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)}\temp\");
+            }
+
+            bitmap.Save($@"{Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData)}\temp\{filename}.jpeg", ImageFormat.Jpeg);
+#endif
 
             //var image = BitMapToBytes(bitmap);
             //var base64 = Convert.ToBase64String(image);
