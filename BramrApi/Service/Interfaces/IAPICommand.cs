dee1e9d1f1ec7625fb92b34aa6ffe53f69b72a63
Boris <Borismulder@outlook.com>
2020-12-17 17:00:21 +0100

    Create IAPICommand.cs

---
+++ BramrApi/Service/Interfaces/IAPICommand.cs
@@ -0,0 +1,16 @@
+﻿using BramrApi.Database.Data;
+using System.Threading.Tasks;
+
+namespace BramrApi.Service.Interfaces
+{
+    public interface IAPICommand
+    {
+        bool CreateWebsiteDirectory(string name);
+
+        void DeleteWebsiteDirectory(string name);
+
+        Task<string> GetIndexFor(string username);
+
+        UserProfile CreateUser(string username);
+    }
+}
