0ec7f6f5631a2d444786b2b6fb932f18cc61d02b
Boris <Borismulder@outlook.com>
2021-01-07 15:23:24 +0100

    Update CvController.cs

--- BramrApi/Controllers/CvController.cs
+++ BramrApi/Controllers/CvController.cs
@@ -1,9 +1,5 @@
 ﻿using BramrApi.Service.Interfaces;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
