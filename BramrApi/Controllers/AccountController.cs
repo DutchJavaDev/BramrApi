f344f7ec4d09719b068aadeea918c935cfa95937
RubenOtter <45627446+RubenOtter@users.noreply.github.com>
2021-01-08 15:03:47 +0100

    Create AccountController

---
+++ BramrApi/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using BramrApi.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BramrApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IDatabase Database;
+        private readonly UserManager<IdentityUser> UserManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, IDatabase database)
+        {
+            UserManager = userManager;
+            Database = database;
+        }
+
+        [HttpGet("info")]
+        [Authorize]
+        public async Task<List<string>> GetUserInfo()
+        {
+            List<string> UserInfo = new List<string>();
+
+            var user = await UserManager.FindByIdAsync(GetIdentity());
+            if(user != null)
+            {
+                try 
+                {
+                    var profile = Database.GetModelByUserName(user.UserName);
+                    UserInfo.Add(user.UserName);
+                    UserInfo.Add($"Joined Bramr on: {profile.CreationDate}");
+#if DEBUG
+                    UserInfo.Add($"https://localhost:44372/cv/{user.UserName}");
+                    UserInfo.Add($"https://localhost:44372/portfolio/{user.UserName}");
+#else
+                    UserInfo.Add($"https://bramr.tech/cv/{user.UserName}");
+                    UserInfo.Add($"https://bramr.tech/portfolio/{user.UserName}");
+#endif
+                    UserInfo.Add(user.Email);
+                    return UserInfo;
+                }
+                catch 
+                {
+                    UserInfo.Clear();
+                    return UserInfo; 
+                }
+            }
+            return UserInfo;
+        }
+
+        [NonAction]
+        private string GetIdentity()
+        {
+            return User.Claims.Where(i => i.Type.Contains("claims/nameidentifier")).FirstOrDefault().Value;
+        }
+    }
+}
